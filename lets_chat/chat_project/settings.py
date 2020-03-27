93b0e8471dc17b16e2ac198b403facb736d919f0
izhan9637 <izhan9637@gmail.com>
2020-03-27 20:27:32 +0530

    Added async consumers

--- lets_chat/chat_project/settings.py
+++ lets_chat/chat_project/settings.py
@@ -74,6 +74,15 @@ WSGI_APPLICATION = 'chat_project.wsgi.application'
 # Channels
 ASGI_APPLICATION = 'chat_project.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
